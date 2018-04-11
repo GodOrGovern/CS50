efd617cc5882f9aa48fb39085582d7b36a785e2c
David Keenan <willkeenan12@gmail.com>
2018-04-11 19:54:07 -0400

    Finished the program, similar in appearance and function to caesar.c

--- pset2/vigenere/vigenere.c
+++ pset2/vigenere/vigenere.c
@@ -1,10 +1,75 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+void encrypt(char lower, char upper, int cipher, char letter);
 
 int main(int argc, string argv[])
 {
+    if (argc != 2)
+    {
+        printf("Error: expected two command line inputs\n");
+        return 1;
+    }
 
+    int len_key = strlen(argv[1]);
+    for (int spot=0; spot < len_key; spot++)
+    {
+        if (!(isalpha(argv[1][spot])))
+        {
+            printf("Key can only contain letters\n");
+            return 1;
+        }
+    }
 
+    string buffer = argv[1];
+    int key[len_key];
+    for(int i = 0; buffer[i]; i++)
+    {
+        buffer[i] = toupper(buffer[i]);
+        key[i] = buffer[i] - 65;
+    }
+
+    printf("plaintext: ");
+    string plaintext = get_string();
+    int len_text = strlen(plaintext);
+    int pos_key = 0;
+    
+    printf("ciphertext: ");
+    for (int pos=0; pos < len_text; pos++)
+    {
+        if (isalpha(plaintext[pos]))
+        {
+            encrypt('a', 'z', key[pos_key % len_key], plaintext[pos]);
+            encrypt('A', 'Z', key[pos_key % len_key], plaintext[pos]);
+            pos_key++;
+        }
+
+        else
+        {
+            printf("%c", plaintext[pos]);
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
+
+// adjusts algorithm for encryption based off letter and key
+void encrypt(char lower, char upper, int cipher, char letter)
+{
+    if (letter >= lower && letter <= upper)
+    {
+        if (letter + cipher <= upper)
+        {
+            printf("%c", letter + cipher);
+        }
+
+        else
+        {
+            int remain = (letter + cipher) - (upper);
+            printf("%c", lower + (remain - 1));
+        }
+    }
 }
