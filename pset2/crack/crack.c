9159d3eaaf5751a200753072430c01461947084c
David Keenan <willkeenan12@gmail.com>
2018-04-21 15:40:37 -0400

    Use recursive function permute() to generate combinations

--- pset2/crack/crack.c
+++ pset2/crack/crack.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int generate(char *permute, int length);
+int permute(char *attempt, char salt[], char *argv [], int pos);
 
 int main(int argc, char *argv[])
 {
@@ -15,30 +15,46 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    char *salt;
+    char salt[2];
     strncpy(salt, argv[1], 2);
 
-    char *strn;
-    int len = 0;
-    while (len < 5)
+    char *attempt;
+    attempt = (char *)malloc(sizeof(char) * 5);
+
+    int pos = 0;
+    while (pos < 5)
     {
-        if(generate(strn, len))
+        if (permute(attempt, salt, argv, pos++))
         {
-            len++;
-        }
-        
-        if (strncmp(crypt(strn, salt), argv[1], 13))
-        {
-            printf("%s\n", strn);
+            printf("%s\n", attempt);
+            free(attempt);
             return 0;
         }
     }
 
+    free(attempt);
     printf("Couldn't find password\n");
     return 1;
 }
 
-int generate(char *permute, int length)
+int permute(char *attempt, char salt[], char *argv[], int pos)
 {
-    
+    char *alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    for (int letter = 0; letter < 52; letter++)
+    {
+        attempt[pos] = alphabet[letter];
+        
+        if (pos > 0)
+        {
+            permute(attempt, salt, argv, pos - 1);
+        }
+
+        if (!(strcmp(crypt(attempt, salt), argv[1])))
+        {
+            return 1;
+        }
+    }
+
+    return 0;
 }
