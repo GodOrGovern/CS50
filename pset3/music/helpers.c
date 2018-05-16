616f38d31cb233c95b3df3f69d79f585997b3218
David Keenan <willkeenan12@gmail.com>
2018-05-16 17:41:09 -0400

    Implemented is_rest(), frequency(), and duration()

--- pset3/music/helpers.c
+++ pset3/music/helpers.c
@@ -7,17 +7,45 @@
 // Converts a fraction formatted as X/Y to eighths
 int duration(string fraction)
 {
-    // TODO
+    float numerator = fraction[0] - '0';
+    float denominator = fraction[2] - '0';
+    float result = numerator / denominator;
+
+    return result / 0.125;
 }
 
 // Calculates frequency (in Hz) of a note
 int frequency(string note)
 {
-    // TODO
+    int length = strlen(note);
+    int *semi = {9, 11, 0, 2, 4, 5, 7};
+    int *pitch = {1, 0, 0, 0, 0, 0, 0, 0, 0, -1};
+    int buffer = semi[note[0] - 65];
+
+    if (length == 2)
+    {
+        buffer += (note[1] - 49) * 12;
+    }
+
+    else
+    {
+        buffer += pitch[(note[1] / 7) - 5] + (note[2] - 48) * 12;
+    }
+
+    int space = abs(45 - buffer);
+    return round(pow(2, space / 12) * 440);
 }
 
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
-    // TODO
+    if (s == "")
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
 }
