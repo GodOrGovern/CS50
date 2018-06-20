6f6443299bfc657d9a5521fa8fccca28f073c65d
David Keenan <willkeenan12@gmail.com>
2018-06-20 20:26:43 +0000

    Make aesthetic changes

--- pset4/whodunit/whodunit.c
+++ pset4/whodunit/whodunit.c
@@ -2,38 +2,38 @@
 #include <cs50.h>
 #include "bmp.h"
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-  	if (argc != 3)
-  	{
-    	fprintf(stderr, "Usage: ./whodunit infile outfile\n");
-    	return 1;
-  	}
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: ./whodunit infile outfile\n");
+        return 1;
+    }
 
-  	char * infile = argv[1];
-  	char * outfile = argv[2];
+    char *infile = argv[1];
+    char *outfile = argv[2];
 
-  	FILE * inpt = fopen(infile, "r");
-	if (inpt == NULL)
-  	{
-    	fprintf(stderr, "Could not open %s\n", infile);
-		return 2;
-  	}
+    FILE *inpt = fopen(infile, "r");
+    if (inpt == NULL)
+    {
+        fprintf(stderr, "Could not open %s\n", infile);
+        return 2;
+    }
 
-	FILE * outpt = fopen(outfile, "w");
-	if (outpt == NULL)
-	{
-		fclose(inpt);
-		fprintf(stderr, "Could not open %s\n", outfile);
-		return 3;
-	}
+    FILE *outpt = fopen(outfile, "w");
+    if (outpt == NULL)
+    {
+        fclose(inpt);
+        fprintf(stderr, "Could not open %s\n", outfile);
+        return 3;
+    }
 
-	BITMAPFILEHEADER bf;
+    BITMAPFILEHEADER bf;
     fread(&bf, sizeof(BITMAPFILEHEADER), 1, inpt);
-	BITMAPINFOHEADER bi;
+    BITMAPINFOHEADER bi;
     fread(&bi, sizeof(BITMAPINFOHEADER), 1, inpt);
 
-	if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
+    if (bf.bfType != 0x4d42 || bf.bfOffBits != 54 || bi.biSize != 40 ||
         bi.biBitCount != 24 || bi.biCompression != 0)
     {
         fclose(outpt);
@@ -42,10 +42,10 @@ int main(int argc, char * argv[])
         return 4;
     }
 
-	fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outpt);
+    fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outpt);
     fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outpt);
 
-    int padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+    int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
 
     for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
     {
@@ -54,12 +54,12 @@ int main(int argc, char * argv[])
             RGBTRIPLE triple;
             fread(&triple, sizeof(RGBTRIPLE), 1, inpt);
 
-			if (triple.rgbtRed == 255)
-			{
-				triple.rgbtRed = 0;
-				triple.rgbtBlue = 255;
-				triple.rgbtGreen = 0;
-			}
+            if (triple.rgbtRed == 255)
+            {
+                triple.rgbtRed = 0;
+                triple.rgbtBlue = 255;
+                triple.rgbtGreen = 0;
+            }
 
             fwrite(&triple, sizeof(RGBTRIPLE), 1, outpt);
         }
@@ -71,7 +71,7 @@ int main(int argc, char * argv[])
         }
     }
 
-	fclose(inpt);
-	fclose(outpt);
-	return 0;
+    fclose(inpt);
+    fclose(outpt);
+    return 0;
 }
