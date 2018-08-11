023232fb14133529ff5824c65a09fcee5e2a6068
David Keenan <willkeenan12@gmail.com>
2018-08-11 14:37:36 -0400

    Fixed issue with loading dictionary

--- pset5-2018/speller/dictionary.c
+++ pset5-2018/speller/dictionary.c
@@ -1,18 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include <stdbool.h>
 #include <string.h>
 
 #include "dictionary.h"
 
-#define ALPHA_LENGTH 27
+#define ALPHA_LEN 27
 #define CHAR_INDEX(c) ((int)(c - 'a'))
 
 typedef struct trie
 {
     bool leaf;
-    struct trie *children[ALPHA_LENGTH];
+    struct trie *children[ALPHA_LEN];
 }
 trie;
 
+int word_count;
+bool loaded = false;
 trie *root;
 
 trie *get_node(void)
@@ -20,9 +25,9 @@ trie *get_node(void)
     trie *node = (trie *)malloc(sizeof(trie));
     node->leaf = false;
 
-    for (int i = 0; i < ALPHA_LENGTH; i++)
+    for (int i = 0; i < ALPHA_LEN; i++)
     {
-        node->character[i] = NULL;
+        node->children[i] = NULL;
     }
 
     return node;
@@ -30,12 +35,12 @@ trie *get_node(void)
 
 void insert(const char *key)
 {
-    int length = strlen(key);
+    int len = strlen(key);
     trie *curr = root;
 
-    for (int level = 0; level < length; level++)
+    for (int level = 0; level < len; level++)
     {
-        int index = CHAR_INDEX(key[level]);
+        int index = isalpha(key[level]) ? CHAR_INDEX(key[level]) : 26;
         if (!curr->children[index])
         {
             curr->children[index] = get_node();
@@ -47,47 +52,36 @@ void insert(const char *key)
     curr->leaf = true;
 }
 
-unsigned int word_count()
+void free_trie(trie *node)
 {
-    int result = 0;
- 
-    if (root->leaf)
+    for (int i = 0; i < ALPHA_LEN; i++) 
     {
-        result++;
-    }
-     
-    for (int i = 0; i < ALPHABET_SIZE; i++)
-    {
-        if (root->children[i])
+        if (node->children[i]) 
         {
-            result += word_count(root->children[i]);
+            free_trie(node->children[i]);
+            node->children[i] = NULL;  
         }
     }
     
-    return result;
-}
-
-void lowercase(char *word)
-{
-    int i = 0;
-    while (word[i])
-    {
-        putchar(tolower(word[i]));
-        i++;
-    }
+    free (node);
 }
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    char *low_word = lowercase(word);
-
+    int len = strlen(word);
+    char low_word[len + 1];
+    for (int i = 0; i < len; i++)
+    {
+        low_word[i] = tolower(word[i]);
+    }
+    
+    low_word[len] = '\0';
     trie *curr = root;
 
-    int length = strlen(low_word);
-    for (int level = 0; level < length; level++)
+    for (int level = 0; level < (len + 1); level++)
     {
-        int index = CHAR_INDEX(word[level]);
+        int index = isalpha(low_word[level]) ? CHAR_INDEX(low_word[level]) : 26;
 
         if (!curr->children[index])
         {
@@ -113,28 +107,34 @@ bool load(const char *dictionary)
     root = (trie *)malloc(sizeof(trie));
 
     char buf[45];
-    while((buf = fgets(buf, 45, dict)) != EOF)
+    while(fgets(buf, 45, dict) != NULL)
     {
+        word_count++;
         insert(buf);
     }
 
-    return false;
+    loaded = true;
+    return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    if (!load())
+    if (!loaded)
     {
         return 0;
     }
 
-    return word_count();
+    return word_count;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // TODO
+    if (root)
+    {
+        free_trie(root);
+        return true;
+    }
     return false;
 }
