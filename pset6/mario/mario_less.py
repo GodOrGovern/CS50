411936d3469eff9c1658b256b0fda219a1d946e8
David Keenan <willkeenan12@gmail.com>
2018-10-30 01:31:17 -0400

    Port of pset1/mario_less.c in python

---
+++ pset6/mario/mario_less.py
@@ -0,0 +1,24 @@
+from cs50 import get_int
+
+
+def main():
+    height = prompt()
+    build(height)    
+
+
+def prompt():
+    height = get_int("Height: ")
+    if height < 0 or height > 23:
+        return prompt()
+    return height
+
+
+def build(height):
+    for row in range(height):
+        [print(' ', end='') for x in range(1, height - row)]
+        [print('#', end='') for n in range(row + 2)]
+        print()
+
+
+if __name__ == '__main__':
+    main()
