93a353cc1414bdde2814517ab5a17f042776cc4b
David Keenan <willkeenan12@gmail.com>
2018-11-12 16:17:23 -0500

    Program to determine edit distance

--- pset6/similarities_more/helpers.py
+++ pset6/similarities_more/helpers.py
@@ -14,6 +14,18 @@ class Operation(Enum):
 
 def distances(a, b):
     """Calculate edit distance from a to b"""
-
-    # TODO
-    return [[]]
+    cost = [[0] * (len(a) + 1)] * (len(b) + 1)
+    cost[0] = [(row, None) for row in range(len(a) + 1)]
+    cost = list(zip(*cost))
+    cost = [list(row) for row in cost]
+    cost[0] = [(col, None) for col in range(len(b) + 1)]
+    for row in range(1, len(a) + 1):
+        for col in range(1, len(b) + 1):
+            cond = [(1, Operation(3).__str__()) if a[row-1] != b[col-1] else (0, None)]
+            trans = [
+                (cost[row-1][col][0] + 1, Operation(1).__str__()),
+                (cost[row][col-1][0] + 1, Operation(2).__str__()),
+                (cost[row-1][col-1][0] + cond[0][0], cond[0][1]),
+            ]
+            cost[row][col] = min(trans, key=lambda x: x[0])
+    return cost
