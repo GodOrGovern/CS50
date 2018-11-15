4017482660ce76f383d8b53514bab882acd1bba2
David Keenan <willkeenan12@gmail.com>
2018-11-14 23:34:44 -0500

    Fix implementation of matrix

--- pset6/similarities_more/helpers.py
+++ pset6/similarities_more/helpers.py
@@ -14,18 +14,19 @@ class Operation(Enum):
 
 def distances(a, b):
     """Calculate edit distance from a to b"""
-    cost = [[0] * (len(a) + 1)] * (len(b) + 1)
-    cost[0] = [(row, None) for row in range(len(a) + 1)]
-    cost = list(zip(*cost))
-    cost = [list(row) for row in cost]
-    cost[0] = [(col, None) for col in range(len(b) + 1)]
+    cost = [[None for x in range(len(b) + 1)] for y in range(len(a) + 1)]
+    for h in range(1, len(a) + 1):
+        cost[h][0] = (h, Operation(1)) 
+    for j in range(1, len(b) + 1):
+        cost[0][j] = (j, Operation(2))
+    cost[0][0] = (0, None)
     for row in range(1, len(a) + 1):
         for col in range(1, len(b) + 1):
-            cond = [(1, Operation(3).__str__()) if a[row-1] != b[col-1] else (0, None)]
+            cond = [1 if a[row-1] != b[col-1] else 0]
             trans = [
-                (cost[row-1][col][0] + 1, Operation(1).__str__()),
-                (cost[row][col-1][0] + 1, Operation(2).__str__()),
-                (cost[row-1][col-1][0] + cond[0][0], cond[0][1]),
+                (cost[row-1][col][0] + 1, Operation(1)),
+                (cost[row][col-1][0] + 1, Operation(2)),
+                (cost[row-1][col-1][0] + cond[0], Operation(3)),
             ]
             cost[row][col] = min(trans, key=lambda x: x[0])
     return cost
