abe52c221d1a7ee2ef58c3e10634c8fe52565571
David Keenan <willkeenan12@gmail.com>
2018-12-05 00:31:31 -0500

    Example files for week8 of 2018 course

---
+++ week8/music/application.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+from cs50 import SQL, eprint
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///lecture.db")
+
+@app.route("/")
+def index():
+    rows = db.execute("SELECT * FROM Album")
+    return render_template("index.html", albums=rows)
