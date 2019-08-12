685c065caedcd026c5d819c8309169b8d979a02a
David Keenan <willkeenan12@gmail.com>
2019-08-12 14:56:05 -0400

    Changed trie to a naive hash table

--- pset5_2018/speller/dictionary.c
+++ pset5_2018/speller/dictionary.c
@@ -7,91 +7,32 @@
 #include "dictionary.h"
 
 #define ALPHA_LEN 27
-#define CHAR_INDEX(c) ((int)(c - 'a'))
+#define CHAR_INDEX(c) ((int)(tolower(c) - 'a'))
 
-typedef struct trie
-{
-    bool leaf;
-    struct trie *children[ALPHA_LEN];
-}
-trie;
-
-int word_count;
+int word_count = 0;
 bool loaded = false;
-trie *root;
+int table[5490] = { 0 };
 
-trie *get_node(void)
+int get_hash(const char* word)
 {
-    trie *node = (trie *)malloc(sizeof(trie));
-    node->leaf = false;
-
-    for (int i = 0; i < ALPHA_LEN; i++)
+    int hash = 0;
+    for (int i = 0; word[i] != '\0' && word[i] != '\n'; i++)
     {
-        node->children[i] = NULL;
+        hash += tolower(word[i]);
     }
 
-    return node;
+    return hash;
 }
 
 void insert(const char *key)
 {
-    int len = strlen(key);
-    trie *curr = root;
-
-    for (int level = 0; level < len; level++)
-    {
-        int index = isalpha(key[level]) ? CHAR_INDEX(key[level]) : 26;
-        if (!curr->children[index])
-        {
-            curr->children[index] = get_node();
-        }
-
-        curr = curr->children[index];
-    }
-
-    curr->leaf = true;
-}
-
-void free_trie(trie *node)
-{
-    for (int i = 0; i < ALPHA_LEN; i++)
-    {
-        if (node->children[i])
-        {
-            free_trie(node->children[i]);
-            node->children[i] = NULL;
-        }
-    }
-
-    free(node);
+    table[get_hash(key)] = true;
 }
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    int len = strlen(word);
-    char low_word[len + 1];
-    for (int i = 0; i < len; i++)
-    {
-        low_word[i] = tolower(word[i]);
-    }
-
-    low_word[len] = '\0';
-    trie *curr = root;
-
-    for (int level = 0; level < (len + 1); level++)
-    {
-        int index = isalpha(low_word[level]) ? CHAR_INDEX(low_word[level]) : 26;
-
-        if (!curr->children[index])
-        {
-            return false;
-        }
-
-        curr = curr->children[index];
-    }
-
-    return (curr != NULL && curr->leaf);
+    return table[get_hash(word)];
 }
 
 // Loads dictionary into memory, returning true if successful else false
@@ -103,8 +44,6 @@ bool load(const char *dictionary)
         fprintf(stderr, "Could not open %s\n", dictionary);
         return false;
     }
-
-    root = get_node();
 
     char buf[LENGTH + 1];
     while (fgets(buf, LENGTH + 2, dict) != NULL)
@@ -121,22 +60,11 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    if (!loaded)
-    {
-        return 0;
-    }
-
-    return word_count;
+    return !loaded ? 0 : word_count;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    if (root)
-    {
-        free_trie(root);
-        return true;
-    }
-
-    return false;
+    return true;
 }
